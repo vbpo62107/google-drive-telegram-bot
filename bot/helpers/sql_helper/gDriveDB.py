8d0d7a51abbb27b45b5e573da046f6f845e0063f
vbpo62107 <vbpo62107@outlook.com>
2025-11-11 13:53:52 +0800

    Add TTL caching to gDrive credential lookup

--- bot/helpers/sql_helper/gDriveDB.py
+++ bot/helpers/sql_helper/gDriveDB.py
@@ -1,5 +1,7 @@
 import pickle
 import threading
+import time
+from cachetools import TTLCache
 from sqlalchemy import Column, Integer, LargeBinary
 from bot.helpers.sql_helper import BASE, get_session
 
@@ -17,6 +19,43 @@ class gDriveCreds(BASE):
 gDriveCreds.__table__.create(checkfirst=True)
 
 INSERTION_LOCK = threading.RLock()
+_CACHE = TTLCache(maxsize=256, ttl=1800)
+
+
+def _cache_key(chat_id):
+    if isinstance(chat_id, bool):
+        return chat_id
+    try:
+        return int(chat_id)
+    except (TypeError, ValueError):
+        return chat_id
+
+
+def _cache_set(chat_id, creds):
+    key = _cache_key(chat_id)
+    if creds is None:
+        _CACHE.pop(key, None)
+    else:
+        _CACHE[key] = (creds, time.time())
+
+
+def _cache_get(chat_id):
+    key = _cache_key(chat_id)
+    entry = _CACHE.get(key)
+    if entry is None:
+        return None
+    return entry[0]
+
+
+def _invalidate_drive(chat_id):
+    try:
+        from bot.modules.drive_helper import invalidate_drive_instance
+    except Exception:
+        return
+    try:
+        invalidate_drive_instance(chat_id)
+    except Exception:
+        pass
 
 
 def _set(chat_id, credential_string):
@@ -31,15 +70,25 @@ def _set(chat_id, credential_string):
             session.add(saved_cred)
             session.commit()
 
+        _cache_set(chat_id, credential_string)
+
+    _invalidate_drive(chat_id)
+
 
 def search(chat_id):
     with INSERTION_LOCK:
+        cached = _cache_get(chat_id)
+        if cached is not None:
+            return cached
+
         with get_session() as session:
             saved_cred = session.query(gDriveCreds).get(chat_id)
             creds = None
             if saved_cred is not None:
                 creds = pickle.loads(saved_cred.credential_string)
-            return creds
+
+        _cache_set(chat_id, creds)
+        return creds
 
 
 def exists(chat_id: str) -> bool:
@@ -55,3 +104,7 @@ def _clear(chat_id):
             if saved_cred:
                 session.delete(saved_cred)
                 session.commit()
+
+        _cache_set(chat_id, None)
+
+    _invalidate_drive(chat_id)
